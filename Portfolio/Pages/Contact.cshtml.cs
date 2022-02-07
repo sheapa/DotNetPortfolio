1e699a80a65b0578d4e6494b79fea69be102a891
sheapa <patshea26@gmail.com>
2022-02-07 18:30:46 -0500

    Bare Bones Portfolio Website

---
+++ Portfolio/Pages/Contact.cshtml.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Portfolio.Pages
+{
+    public class ContactModel : PageModel
+    {
+        private readonly ILogger<ContactModel> _logger;
+
+        public ContactModel(ILogger<ContactModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Portfolio/Pages/MyApps.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Portfolio.Pages
+{
+    public class MyAppsModel : PageModel
+    {
+        private readonly ILogger<MyAppsModel> _logger;
+
+        public MyAppsModel(ILogger<MyAppsModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
